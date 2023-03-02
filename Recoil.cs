515d3f4abb46a78c50ecb11dbcce952da17d31d6
lucent15 <125958044+lucent15@users.noreply.github.com>
2023-03-03 06:28:19 +0900

    Add files via upload

---
+++ Recoil.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recoil : MonoBehaviour
+{
+
+    //Rotations
+    private Vector3 currentRotation;
+    private Vector3 targetRotation;
+
+    //Hipfire Recoil
+    [SerializeField] private float recoilX;
+    [SerializeField] private float recoilY;
+    [SerializeField] private float recoilZ;
+
+    //settings
+    [SerializeField] private float snappiness;
+    [SerializeField] private float returnSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetRotation = Vector3.Lerp(targetRotation,Vector3.zero,returnSpeed*Time.deltaTime);
+        currentRotation = Vector3.Slerp(currentRotation,targetRotation,snappiness*Time.deltaTime);
+        transform.localRotation = Quaternion.Euler(currentRotation);
+    }
+
+    public void RecoilFire()
+    {
+        targetRotation += new Vector3(recoilX,Random.Range(-recoilY,recoilY),Random.Range(-recoilZ,recoilZ));
+    }
+
+    public void UpRecoilParam()
+    {
+        snappiness = 5;
+        returnSpeed = 8;
+    }
+
+    public void ResetRecoilParam() {
+        snappiness = 8;
+        returnSpeed = 4;
+    }
+}
---
+++ ShakeObjectsByDOTween.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;  // これを追記
+
+// アセット DOTween を使ってゲームオブジェクトをゆらすサンプル
+// 解説記事　http://negi-lab.blog.jp/ShakeObjectsByDOTween
+// DOTween Documentation  http://dotween.demigiant.com/documentation.php
+public class ShakeObjectsByDOTween : MonoBehaviour
+{
+    [SerializeField] GameObject[] shakeObjects; // 揺らすオブジェクト
+    [SerializeField] float duration;            // 揺れる時間
+    [SerializeField] float strength = 1f;       // 揺れる幅
+    [SerializeField] int vibrato = 10;          // 揺れる回数
+    [SerializeField] float randomness = 90f;    // Indicates how much the shake will be random (0 to 180 ...
+    [SerializeField] bool snapping = false;     // If TRUE the tween will smoothly snap all values to integers. 
+    [SerializeField] bool fadeOut = false;       // 徐々に揺れが収まるか否か
+
+    /*   private void Update()
+       {
+           if (Input.anyKeyDown)
+           {
+               Shake(shakeObjects);
+           }
+       }*/
+
+    // DOTweenでオブジェクトをゆらす
+    public void Shaketes(GameObject[] shakeObjects)
+    {
+        foreach (var shakeObject in shakeObjects)
+        {
+            shakeObject.transform.DOShakePosition(
+                duration, strength, vibrato, randomness, snapping, fadeOut);
+            // DOShakePosition は duration 以外の引数はオプション（指定しなければデフォルト値使用）
+            // なので、以下のようにシンプルに書くこともできる
+            // shakeObject.transform.DOShakePosition ( duration );
+        }
+    }
+    public void Shake(int shakernum)
+    {
+        var defopos = shakeObjects[shakernum].transform.position;
+        shakeObjects[shakernum].transform.DOShakePosition(
+            duration, strength, vibrato, randomness, snapping, fadeOut);
+        // DOShakePosition は duration 以外の引数はオプション（指定しなければデフォルト値使用）
+        // なので、以下のようにシンプルに書くこともできる
+        // shakeObject.transform.DOShakePosition ( duration );
+        if(shakernum==0)shakeObjects[shakernum].transform.localPosition = new Vector3(0,0,0);
+
+    }
+}
